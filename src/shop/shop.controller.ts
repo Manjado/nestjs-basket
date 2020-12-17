aa728840ffedd2f1045734d77c7083d03aa7ebfe
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-12-17 18:00:37 +0100

    lifecycle-events and scoope

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -5,40 +5,30 @@ import {
   Inject,
   Param,
   Redirect,
+  Scope,
 } from '@nestjs/common';
 import { GetListOfProductsResponse } from 'src/interfaces/shop';
 import { ShopService } from './shop.service';
 
-// @Controller({
-//   path: 'shop',
-//   host: 'zzz.lvh.me',
-// })
-
 @Controller({
   path: 'shop',
-  host: ':name.lvh.me',
+  scope: Scope.REQUEST,
 })
 export class ShopController {
+  onApplicationBootstrap() {
+    console.log('załadowany!');
+  }
+
+  onApplicationShutdown() {
+    console.log('Aplikacja zaraz zniknie');
+  }
+
   constructor(@Inject(ShopService) private shopService: ShopService) {}
 
   @Get('/')
   getListOfProduct(): GetListOfProductsResponse {
     return this.shopService.getProducts();
   }
-
-  // @Get('/test')
-  // @Redirect('/test2')
-  // testRedirect() {}
-
-  // @Get('/test/:age')
-  // @Redirect()
-  // testRedirect(@Param('age') age: string) {
-  //   const url = Number(age) > 18 ? '/site' : '/block';
-  //   return {
-  //     url,
-  //     statusCode: 301,
-  //   };
-  // }
 
   @Get('/welcome')
   welcome(@HostParam('name') siteName: string): string {
