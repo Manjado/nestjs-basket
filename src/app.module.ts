6e06905bc5c1cba9aa50cdfedfe034e0beb02c0a
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-12-20 15:21:46 +0100

    entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BasketModule } from './basket/basket.module';
 import { ShopModule } from './shop/shop.module';
 
 @Module({
-  imports: [BasketModule, ShopModule],
+  imports: [TypeOrmModule.forRoot(), BasketModule, ShopModule],
   controllers: [AppController],
   providers: [AppService],
 })
