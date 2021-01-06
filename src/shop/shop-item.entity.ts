f1340f04720fbad6c8bb78d416acf0af9669bede
malchimowicz <michal.alchimowicz@enovatis.pl>
2021-01-06 17:51:11 +0100

    one-to-one

--- src/shop/shop-item.entity.ts
+++ src/shop/shop-item.entity.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ShopItemDetails } from './shop-item-details.entity';
 
 @Entity()
 export class ShopItem extends BaseEntity {
@@ -38,4 +46,8 @@ export class ShopItem extends BaseEntity {
     default: false,
   })
   wasEverBought: boolean;
+
+  @OneToOne((type) => ShopItemDetails)
+  @JoinColumn()
+  details: ShopItemDetails;
 }
