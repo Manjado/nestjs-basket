e8bced37b1a3b7cd58ba55a09b8d8ecd125d20c4
malchimowicz <michal.alchimowicz@enovatis.pl>
2021-05-09 10:58:19 +0200

    nest g resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { BasketModule } from './basket/basket.module';
 import { ShopModule } from './shop/shop.module';
 import { CacheModule } from './cache/cache.module';
+import { DiscountCodeModule } from './discount-code/discount-code.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), BasketModule, ShopModule, CacheModule],
+  imports: [TypeOrmModule.forRoot(), BasketModule, ShopModule, CacheModule, DiscountCodeModule],
   controllers: [AppController],
   providers: [AppService],
 })
